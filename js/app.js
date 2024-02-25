f19b39facc100bb1c158d11b806cdb5657502ad0
Đỗ Cao Long <long@myspa.vn>
2024-02-25 23:12:36 +0700

    [UPDATE] slider

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const firstNavItem = document.querySelector(".nav ul li:first-child a");
     const megaMenu = document.getElementById("mega-menu");
     const navItems = document.querySelectorAll(".nav ul li:not(:first-child)");
-    const megaMenuInner = document.querySelector(".l-mega-menu__inner");
+    const megaMenuInner = document.querySelector(".mega-menu__inner");
     let isHovered = false;
     firstNavItem.addEventListener("mouseenter", function () {
         megaMenu.classList.add("is-active");
@@ -28,4 +28,39 @@ document.addEventListener("DOMContentLoaded", function () {
         megaMenu.classList.remove("is-active");
         firstNavItem.classList.remove("active");
     });
+});
+
+// slider
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    autoplayDisableOnInteraction: false,
+    slidesPerView: 1,
+    autoHeight: true,
+    autoplay: {
+        delay: 5000,
+    },
+    // autoplay: false,
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: "true",
+        type: "bullets",
+        renderBullet: function (index, className) {
+            return (
+                '<span class="' +
+                className +
+                '">' +
+                "<i></i>" +
+                "<b></b>" +
+                "</span>"
+            );
+        },
+    },
+    navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+    },
 });
