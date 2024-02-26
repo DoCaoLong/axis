ad98076385b98590803140ca9bec6eae35b2ccb0
Đỗ Cao Long <long@myspa.vn>
2024-02-26 22:56:09 +0700

    [UPDATE] RESPONSIVE

--- js/app.js
+++ js/app.js
@@ -28,6 +28,55 @@ document.addEventListener("DOMContentLoaded", function () {
         megaMenu.classList.remove("is-active");
         firstNavItem.classList.remove("active");
     });
+
+    // menu mobile active
+
+    function animations() {
+        const menuToggleOpen = document.querySelector(".hambuger");
+        const menuToggleClose = document.querySelector(".close-nav");
+        // const menu = document.querySelector(".nav");
+
+        const tl = gsap.timeline({
+            paused: true,
+        });
+        if (window.innerWidth <= 1023) {
+            tl.to(".nav, .nav-outer", {
+                x: 0,
+                duration: 0.6,
+                ease: "expo.inOut",
+            });
+
+            tl.from(".nav ul li", {
+                y: 40,
+                opacity: 0,
+                duration: 0.4,
+                ease: "expo.out",
+                stagger: 0.1,
+            });
+
+            tl.from(".contact", {
+                y: 40,
+                opacity: 0,
+                duration: 0.4,
+                ease: "expo.out",
+            });
+            tl.from(".social-wrap", {
+                y: 40,
+                opacity: 0,
+                duration: 0.4,
+                ease: "expo.out",
+            });
+            menuToggleOpen.addEventListener("click", () => tl.play());
+            menuToggleClose.addEventListener("click", () => tl.reverse());
+        }
+    }
+    window.addEventListener("resize", animations);
+    animations();
+});
+
+const activeSubmenuMB = document.querySelector(".mb-item");
+activeSubmenuMB.addEventListener("click", () => {
+    activeSubmenuMB.classList.toggle("active-sub");
 });
 
 // slider
@@ -35,7 +84,7 @@ const swiper = new Swiper(".swiper", {
     loop: true,
     autoplayDisableOnInteraction: false,
     slidesPerView: 1,
-    autoHeight: true,
+    // autoHeight: true,
     autoplay: {
         delay: 5000,
     },
